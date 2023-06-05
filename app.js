13f83a6a4b47a89dce83a2179ada9f4b7e71409e
MatthiasBlc <mat.bouloc@gmail.com>
2023-06-05 10:00:22 +0200

    init

--- app.js
+++
@@ -1,3 +0,0 @@
-import { createReadStream } from "node:fs";
-
-const stream = createReadStream("video.mp4");
---
+++ appStream.js
@@ -0,0 +1,24 @@
+// --------------------------- READSTREAM ---------------------------
+
+// import { createReadStream } from "node:fs";
+// import { stat } from "node:fs/promises";
+
+// const stream = createReadStream("video.mp4");
+// const { size } = await stat("video.mp4");
+// let read = 0;
+// stream.on("data", (chunk) => {
+//   read += chunk.length;
+//   console.log(Math.round((100 * read) / size));
+// });
+
+// stream.on("close", () => {
+//   console.log("close");
+// });
+
+// --------------------------- WRITESTREAM ---------------------------
+
+import { createReadStream, createWriteStream } from "node:fs";
+
+const stream = createReadStream("video.mp4");
+const writeStream = createWriteStream("video-copy.mp4");
+stream.pipe(writeStream);
